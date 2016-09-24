1fd681ed5eebf3cdaace2a24e05c931afed1fd67
Cornelius Carl <corneliuscarl@me.com>
2016-09-24 20:58:01 +0200

    todo: move outcommented code to a class attached to the collider

--- Assets/SwingFeedback.cs
+++ Assets/SwingFeedback.cs
@@ -30,21 +30,24 @@ public class SwingFeedback : MonoBehaviour {
 		controller.TriggerHapticPulse((ushort)Mathf.Lerp(0, 2000, strengthPercentage));
 	}
 
-	void OnCollisionEnter(Collision collision)
-	{
-		StartCoroutine(LongVibration(0.4f, 200));
-	}
+	//void OnCollisionEnter(Collision collision)
+	//{
+	//	if (collision.collider.name.Equals("Ball"))
+	//	{
+	//		StartCoroutine(LongVibration(0.4f, 200));
+	//	}
+	//}
 
-	// taken from https://steamcommunity.com/app/358720/discussions/0/405693392914144440/
-	// TriggerHapticPulse takes in the strength but will trigger only for one frame
-	IEnumerator LongVibration(float length, float strength)
-	{
-		for (float i = 0; i < length; i += Time.deltaTime)
-		{ 
-			controller.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
-			yield return null;
-		}
-	}
+	//// taken from https://steamcommunity.com/app/358720/discussions/0/405693392914144440/
+	//// TriggerHapticPulse takes in the strength but will trigger only for one frame
+	//IEnumerator LongVibration(float length, float strength)
+	//{
+	//	for (float i = 0; i < length; i += Time.deltaTime)
+	//	{ 
+	//		controller.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+	//		yield return null;
+	//	}
+	//}
 
 
 }
