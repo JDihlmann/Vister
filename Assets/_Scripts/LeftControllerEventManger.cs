27681d02fbb4bb224897265ec31ab0c3fa7a4497
Jan-Niklas Dihlmann <m.dihlmann@googlemail.com>
2016-09-24 20:57:47 +0200

    Added basic textures and tested gravity effect

---
+++ Assets/_Scripts/LeftControllerEventManger.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftControllerEventManger : MonoBehaviour {
+
+    // Event Handler
+    public delegate void rightButtonUp();
+    public static event rightButtonUp onLeftButtonUp;
+
+    public delegate void leftButtonDown(Vector3 position);
+    public static event leftButtonDown onLeftButtonDown;
+
+
+    // VR Handler
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private Valve.VR.EVRButtonId touchpadButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+
+
+    public bool triggerButtonUp = false;
+    public bool triggerButtonDown = false;
+    public bool triggerButtonPressed = false;
+    public bool touchpadButtonPressed = false;
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+
+    private Transform rotortransform;
+
+
+    void Start() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void FixedUpdate() {
+        if(controller == null) {
+            Debug.Log("Controller not reachable");
+            return;
+        }
+        
+
+        triggerButtonUp = controller.GetPressUp(triggerButton);
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        triggerButtonPressed = controller.GetPress(triggerButton);
+
+        
+        if (triggerButtonUp && onLeftButtonUp != null) {
+            Debug.Log("Left Up");
+            onLeftButtonUp();
+        }
+
+        if (triggerButtonDown && onLeftButtonDown != null) {
+            Debug.Log("Left Down");
+            onLeftButtonDown(transform.position); 
+        }
+
+        if (triggerButtonPressed && onLeftButtonDown != null) {
+            onLeftButtonDown(transform.position);
+        }
+    }
+
+}
+
+
+
---
+++ Assets/_Scripts/Magnet.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnet : MonoBehaviour {
+
+    private Rigidbody rgbd;
+    public float velocityMultiplier;
+    
+	void Start () {
+        rgbd = transform.GetComponent<Rigidbody>();
+	}
+
+    void OnEnable() {
+        LeftControllerEventManger.onLeftButtonDown += SetVelocity;
+
+    }
+
+    void OnDisable() {
+        LeftControllerEventManger.onLeftButtonDown += SetVelocity;
+    }
+
+	
+	void SetVelocity (Vector3 position) {
+       
+        Vector3 velocity = position - transform.position;
+
+       
+        
+        if(velocity.magnitude >= 1) {
+            float multiplier = Mathf.Exp(velocity.magnitude);
+            rgbd.velocity = velocity * multiplier;
+        } 
+        
+        
+        
+    }
+}
