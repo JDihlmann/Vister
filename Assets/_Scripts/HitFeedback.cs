8c578abe63c2f7c0eb5b1e0bd266c1eb2062d7c3
Cornelius Carl <corneliuscarl@me.com>
2016-09-25 16:16:18 +0200

    added hitfeedback, removed swingfeedback log

---
+++ Assets/_Scripts/HitFeedback.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitFeedback : MonoBehaviour {
+
+	public SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.name.Equals("Ball"))
+		{
+			StartCoroutine(LongVibration(0.4f, 200));
+		}
+	}
+
+	// taken from https://steamcommunity.com/app/358720/discussions/0/405693392914144440/
+	// TriggerHapticPulse takes in the strength but will trigger only for one frame
+	IEnumerator LongVibration(float length, float strength)
+	{
+		for (float i = 0; i < length; i += Time.deltaTime)
+		{
+			controller.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+			yield return null;
+		}
+	}
+
+}
--- Assets/_Scripts/SwingFeedback.cs
+++ Assets/_Scripts/SwingFeedback.cs
@@ -26,36 +26,14 @@ public class SwingFeedback : MonoBehaviour {
 		// calculate airdrag with coeeficent (guessed), air density on earth (ca 1.3), the biggest area of the collider and the velocity ^ 2
 		float airDrag = 0.5f / 2 * 1.3f * Mathf.Max(racketCollider.size.x, racketCollider.size.y, racketCollider.size.z) * fakeVelocity * fakeVelocity;
 
-		float strengthPercentage = Mathf.Abs(1 - (racketForce - airDrag));
-		Debug.Log(strengthPercentage);
+		float strength = Mathf.Abs(1 - (racketForce - airDrag));
 
-		if (strengthPercentage >= 500)
+		if (strength >= 500)
 		{
-			controller.TriggerHapticPulse((ushort)strengthPercentage);
-		} 
-		//controller.TriggerHapticPulse((ushort)Mathf.Lerp(0, 2000, strengthPercentage));
+			controller.TriggerHapticPulse((ushort)strength);
+		}
 
 		lastPoint = transform.position;
-	}
-
-	//void OnCollisionEnter(Collision collision)
-	//{
-	//	if (collision.collider.name.Equals("Ball"))
-	//	{
-	//		StartCoroutine(LongVibration(0.4f, 200));
-	//	}
-	//}
-
-	//// taken from https://steamcommunity.com/app/358720/discussions/0/405693392914144440/
-	//// TriggerHapticPulse takes in the strength but will trigger only for one frame
-	//IEnumerator LongVibration(float length, float strength)
-	//{
-	//	for (float i = 0; i < length; i += Time.deltaTime)
-	//	{ 
-	//		controller.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
-	//		yield return null;
-	//	}
-	//}
-
+	}	
 
 }
