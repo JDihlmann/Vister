6ed5fc37e4694553ac6131f955b8e90010f58fd2
Jan-Niklas Dihlmann <m.dihlmann@googlemail.com>
2016-09-29 17:15:36 +0200

    Changed bouncyness of ball should be better now. Still need some tweaks here and there.

---
+++ Assets/BallHeightMeassure.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BallHeightMeassure : MonoBehaviour {
+
+
+    private List<float> pos;
+
+	// Use this for initialization
+	void Start () {
+        pos = new List<float>();
+    }
+    
+    void FixedUpdate() {
+        pos.Add(transform.position.y);
+    }
+
+
+    void OnCollisionEnter(Collision collided) {
+
+        float max = 0;
+
+        foreach (float item in pos) {
+            if(item > max) {
+                max = item;
+            }
+        }
+
+        pos.Clear();
+        Debug.Log("Max height: " + max);
+    }
+}
